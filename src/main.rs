18caf95bcc2598eb2932339f93f2933ebd617351
apetrovic6 <petrovicante6@gmail.com>
2025-03-11 19:31:12 +0100

    return 404 on a route

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::{
+    io::{stdout, BufRead, BufReader, Read, Write},
+    net::{Shutdown, TcpStream},
+    str::Lines,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,16 +15,49 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let str = String::from("HTTP/1.1 200 OK\r\n\r\n");
-
-                let buf = str.into_bytes();
-
-                let res = _stream.write_all(buf.as_slice());
-                println!("200 returned");
+                println!("in stream");
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+
+    let req: Vec<_> = buf_reader
+        .lines()
+        .map(|res| res.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    match req.join("").contains(&String::from("/abcdefg")) {
+        true => send_404(&mut stream),
+        false => send_200(&mut stream),
+    }
+}
+
+fn send_200(stream: &mut TcpStream) {
+    let str = String::from("HTTP/1.1 200 OK\r\n\r\n");
+
+    let buf = str.into_bytes();
+
+    println!("sending 200");
+    if let Err(e) = stream.write_all(buf.as_slice()) {
+        eprintln!("Failed to write response: {:?}", e); // Prevent shutdown on a failed write
+    }
+}
+
+fn send_404(stream: &mut TcpStream) {
+    let str = String::from("HTTP/1.1 404 Not Found\r\n\r\n");
+
+    let buf = str.into_bytes();
+
+    println!("sending 404");
+    if let Err(e) = stream.write_all(buf.as_slice()) {
+        eprintln!("Failed to write response: {:?}", e); // Prevent shutdown on a failed write
     }
 }
