af6658d623d79eec3cd2a0e7a257c200b9a98c73
apetrovic6 <petrovicante6@gmail.com>
2025-04-05 11:17:46 +0200

    revert

--- src/models/encoding.rs
+++ src/models/encoding.rs
@@ -1,20 +1,21 @@
+use std::str::FromStr;
 use strum::{Display, EnumString};
 
 #[derive(Display, Debug, Hash, Eq, PartialEq, EnumString)]
 pub enum EncodingType {
-    #[strum(to_string = "gzip", serialize = "gzip")]
+    #[strum(to_string = "gzip")]
     Gzip,
-    #[strum(to_string = "", serialize = "")]
+    #[strum(to_string = "")]
     None,
 }
 
-// impl FromStr for EncodingType {
-//     type Err = &'static str;
-//
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {
-//         match s.to_lowercase().as_str() {
-//             "gzip" => Ok(EncodingType::Gzip),
-//             _ => Ok(EncodingType::None),
-//         }
-//     }
-// }
+impl FromStr for EncodingType {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "gzip" => Ok(EncodingType::Gzip),
+            _ => Ok(EncodingType::None),
+        }
+    }
+}
