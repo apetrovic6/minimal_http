d375a13b13d399075bb81c49dd5a5159804afd7d
apetrovic6 <petrovicante6@gmail.com>
2025-04-05 11:00:26 +0200

    tests

--- src/models/encoding.rs
+++ src/models/encoding.rs
@@ -1,27 +1,34 @@
-use std::{fmt::Display, str::FromStr};
+use strum::Display;
 
-#[derive(Debug, Hash, Eq, PartialEq)]
+#[derive(Display, Debug, Hash, Eq, PartialEq)]
 pub enum EncodingType {
+    #[strum(to_string = "gzip")]
     Gzip,
     None,
 }
 
-impl FromStr for EncodingType {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "gzip" => Ok(EncodingType::Gzip),
-            _ => Ok(EncodingType::None),
-        }
-    }
-}
-
-impl Display for EncodingType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            EncodingType::Gzip => write!(f, "gzip"),
-            EncodingType::None => write!(f, ""),
-        }
-    }
-}
+// impl EncodingType {
+//     fn description(&self) -> &'static str {
+//         match self {
+//             EncodingType::Gzip => "gzip",
+//             EncodingType::None => "",
+//         }
+//     }
+// }
+//
+// impl FromStr for EncodingType {
+//     type Err = &'static str;
+//
+//     fn from_str(s: &str) -> Result<Self, Self::Err> {
+//         match s.to_lowercase().as_str() {
+//             "gzip" => Ok(EncodingType::Gzip),
+//             _ => Ok(EncodingType::None),
+//         }
+//     }
+// }
+//
+// impl Display for EncodingType {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "{}", self.description())
+//     }
+// }
